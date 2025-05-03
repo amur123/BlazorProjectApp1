0662fad09e76d56422a020903a5e979a5c05c86b
amur123 <188503228+amur123@users.noreply.github.com>
2025-05-03 20:52:55 +0100

    style: linter JS styling fix; encoding to UTF-8 (no BOM), single spaced comments

--- wwwroot/js/playAudio.js
+++ wwwroot/js/playAudio.js
@@ -1,10 +1,10 @@
-﻿// Javascript for audio player
+// Javascript for audio player
 window.audioPlayer = {
     audio: null,
 
     play: function (url) {
         if (this.audio) {
-            this.audio.play()   // If paused it continues playing
+            this.audio.play() // If paused it continues playing
             return
         }
         this.audio = new Audio(url)
@@ -13,14 +13,14 @@ window.audioPlayer = {
 
     pause: function () {
         if (this.audio) {
-            this.audio.pause()  // Pauses without resetting position
+            this.audio.pause() // Pauses without resetting position
         }
     },
 
     stop: function () {
         if (this.audio) {
-            this.audio.currentTime = 0      // Set to start from position 0
-            this.audio.pause()              // Pauses at the beginning
+            this.audio.currentTime = 0 // Set to start from position 0
+            this.audio.pause() // Pauses at the beginning
         }
     }
 }
