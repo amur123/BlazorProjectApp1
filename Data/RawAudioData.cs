1db1195d142055a7913cf89c13c9cd64c9474c97
amur123 <188503228+amur123@users.noreply.github.com>
2025-05-03 23:44:31 +0100

    style: Styling fix for linter.

--- Data/RawAudioData.cs
+++ Data/RawAudioData.cs
@@ -5,13 +5,12 @@ namespace BlazorProjectApp1.Data;
 internal class RawAudioData
     {
     [Key]
-    public int PostId { get; set; }     // ID which is linked to a post.
+    public int PostId { get; set; } // ID which is linked to a post.
 
     public int AudioId { get; set; }
 
     [Required]
-    public byte[] AudioBinaryData { get; set; } = Array.Empty<byte>();       // Property for RAW audio data.
+    public byte[] AudioBinaryData { get; set; } = Array.Empty<byte>(); // Property for RAW audio data.
 
-    public Post RelevantPost { get; set; } = null!;         // Property for navigating to the related post.
+    public Post RelevantPost { get; set; } = null!; // Property for navigating to the related post.
 }
-
